3707604bc8010448ad4ff2f6d248581c64362312
shubham98630-bit <shubham98630@gmail.com>
2026-06-22 10:51:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -351,7 +351,9 @@ def get_incoming_stock_history():
         return jsonify([dict(l) for l in logs])
     finally:
         conn.close()
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
     
